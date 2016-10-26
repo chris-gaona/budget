911b41b352a0b5db36818724f21101813685ab36
Chris Gaona <thebeloved88@sbcglobal.net>
2016-10-25 20:33:07 -0700

    Adds some initial e2e tests for app

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,18 @@
-import { BudgetPage } from './app.po';
+import {browser, element, by, protractor} from "protractor";
 
-describe('budget App', function() {
-  let page: BudgetPage;
-
-  beforeEach(() => {
-    page = new BudgetPage();
+describe('Budget App', function() {
+  it('should have a title', () => {
+    browser.get('/');
+    expect(browser.getTitle()).toEqual('TrackBudget');
   });
 
-  it('should display message saying Budget Tracker', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Budget Tracker');
+  it('should display a message for main app title', () => {
+    expect(element(by.css('.display-4')).getText()).toEqual('Budget Tracker');
   });
+
+  it('should display a simple sub title', () => {
+    expect(element(by.css('.lead')).getText()).toEqual('This is a simple budget tracker using Bootstrap 4 and Angular 2!');
+  });
+
+  it('should...');
 });
