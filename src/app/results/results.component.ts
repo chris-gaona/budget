c9a29ebec3ecb49f5cfc0cac6c6a13e9c14ed28c
Chris Gaona <thebeloved88@sbcglobal.net>
2016-10-25 20:22:18 -0700

    Moves results html to it's own component file again

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-results',
@@ -6,6 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+
+  @Input() totalSpent: number;
 
   constructor() { }
 
