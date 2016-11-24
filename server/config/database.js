b6046b3464c417fc80996bbcd2cb8d78d9d2bf8e
Chris Gaona <thebeloved88@sbcglobal.net>
2016-11-24 12:47:45 -0800

    Fixes database config file for Heroku push

--- server/config/database.js
+++ server/config/database.js
@@ -8,11 +8,18 @@ var configFile = require('./config.json');
 var config = {};
 var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
 var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
+var prodDB;
+
+if (MODULUS_USERNAME !== undefined && MODULUS_PASSWORD !== undefined) {
+  prodDB = 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@jello.modulusmongo.net:27017/xabeGi8m';
+} else {
+  prodDB = configFile.production;
+}
 
 config.mongoURI = {
   development: 'mongodb://localhost/budget',
   test: 'mongodb://localhost/budget-test',
-  production: configFile.production
+  production: prodDB
 };
 
 module.exports = config;
