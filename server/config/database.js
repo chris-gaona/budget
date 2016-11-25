05a379d2cc8e666a380fe599a4fa0a432a73f991
Chris Gaona <thebeloved88@sbcglobal.net>
2016-11-24 18:27:09 -0800

    Attempts to change database config file to fix deploy issues

--- server/config/database.js
+++ server/config/database.js
@@ -8,18 +8,18 @@ var configFile = require('./config.json');
 var config = {};
 var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
 var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
-var prodDB;
-
-if (MODULUS_USERNAME !== undefined && MODULUS_PASSWORD !== undefined) {
-  prodDB = 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@olympia.modulusmongo.net:27017/iW3otapu';
-} else {
-  prodDB = configFile.production;
-}
+// var prodDB;
+//
+// if (MODULUS_USERNAME !== undefined && MODULUS_PASSWORD !== undefined) {
+//   prodDB = 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@olympia.modulusmongo.net:27017/iW3otapu';
+// } else {
+//   prodDB = configFile.production;
+// }
 
 config.mongoURI = {
   development: 'mongodb://localhost/budget',
   test: 'mongodb://localhost/budget-test',
-  production: prodDB
+  production: 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@olympia.modulusmongo.net:27017/iW3otapu'
 };
 
 module.exports = config;
