189e61e0ca3711622ab3d9242ff49a11f050010d
Chris Gaona <thebeloved88@sbcglobal.net>
2016-11-27 10:17:19 -0800

    Fixes bug with newly added validation for create/edit budget

--- server/app.js
+++ server/app.js
@@ -72,25 +72,23 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: {}
+    error: err
   });
 });
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json({
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 
 module.exports = app;
