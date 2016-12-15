c6747b3253b30776f9404af2b60d760c38e4cc5f
Chris Gaona <thebeloved88@sbcglobal.net>
2016-12-14 20:17:52 -0800

    Removes files from git that have been recently added to .gitignore

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-// requires all needed files in this folder
-var budgets_1 = require('./budgets');
-exports.budget = budgets_1.budget;
-var auth_1 = require('./auth');
-exports.auth = auth_1.auth;
-var users_1 = require('./users');
-exports.users = users_1.users;
